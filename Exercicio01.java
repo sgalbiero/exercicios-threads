8cf81f2a0e4806d3b54882dd4b5b9b5e49d1b05f
Vinicius Sgalbiero <vsgalbiero@gmail.com>
2026-08-19 20:02:15 -0300

    Add Exercicio01.java to print 'Olá, mundo!'

---
+++ Exercicio01.java
@@ -0,0 +1,15 @@
+public class Exercicio01 {
+    public static void main(String[] args) {
+        Thread thread = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Olá, mundo!");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+    }
+}
